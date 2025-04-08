3d5e33b8fca1893d198c5f2517f65e9e48c9e4a3
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2025-04-08 21:51:39 +0100

    Add sample data for users

---
+++ db/data.sql
@@ -0,0 +1,11 @@
+﻿-- Add sample users
+
+INSERT INTO role (role_name) VALUES
+('operations_manager'),
+('administrator'),
+('env_scientist');
+
+INSERT INTO user (first_name, last_name, email, role_id) VALUES
+('John', 'Manager', 'john.manager@example.com', 1),
+('Sarah', 'Admin', 'sarah.admin@example.com', 2),
+('Michael', 'Scientist', 'michael.scientist@example.com', 3);
