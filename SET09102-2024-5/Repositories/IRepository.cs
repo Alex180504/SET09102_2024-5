da96e4c0f3077b8e40c54eaac5cc592ba9462d95
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2025-04-13 23:08:45 +0100

    Add data repository

---
+++ SET09102-2024-5/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace SET09102_2024_5.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        Task<int> SaveChangesAsync();
+    }
+}
