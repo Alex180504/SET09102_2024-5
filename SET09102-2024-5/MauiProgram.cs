05aa8eb1acc6ed4b3f7b041620fa39426b63cf76
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2025-04-13 23:09:24 +0100

    Add prototype db service

--- SET09102-2024-5/MauiProgram.cs
+++ SET09102-2024-5/MauiProgram.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using SET09102_2024_5.Data;
+using SET09102_2024_5.Data.Repositories;
+using SET09102_2024_5.Services;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace SET09102_2024_5
 {
@@ -15,8 +20,20 @@ namespace SET09102_2024_5
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            // Configure database
+            var connectionString = "Server=localhost;Database=sensor_monitoring;User=your_username;Password=your_password;";
+
+            builder.Services.AddDbContext<SensorMonitoringContext>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            // Register repositories
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            // Register services
+            builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
---
+++ SET09102-2024-5/Services/IDatabaseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SET09102_2024_5.Services
+{
+    public interface IDatabaseService
+    {
+        Task InitializeDatabaseAsync();
+        Task<bool> TestConnectionAsync();
+    }
+}
