642bed46fa08f0a79dccb27a010fb0d7d66ba9ea
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2025-04-21 18:23:59 +0100

    Add converters

---
+++ SET09102-2024-5/Converters/BoolToHeightConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace SET09102_2024_5.Converters
+{
+    public class BoolToHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isVisible && isVisible)
+            {
+                // When visible, use the parameter as height
+                if (parameter is string heightStr && double.TryParse(heightStr, out double height))
+                {
+                    return height;
+                }
+                return 200; // Default height
+            }
+
+            // When not visible, return a zero height
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SET09102-2024-5/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace SET09102_2024_5.Converters
+{
+    // InvertedBoolConverter is a value converter that inverts a boolean value.
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            return value;
+        }
+    }
+}
