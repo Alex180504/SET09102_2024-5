e5a0c40bd7aead87112fdefaab20dc48e4d42446
Illia <illiadovho@gmail.com>
2025-04-25 11:46:28 +0100

    Added timer for async data retrieval and sensor service to retrieve data

---
+++ SET09102-2024-5/Services/PollingTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SET09102_2024_5.Services
+{
+    public class PollingTimer : IDisposable
+    {
+        private Timer? _timer;
+        public TimeSpan Interval { get; set; } = TimeSpan.FromSeconds(5);
+        public event Action? OnTick;
+
+        public Task StartAsync()
+        {
+            // fire immediately then every Interval
+            _timer = new Timer(_ => OnTick?.Invoke(), null, TimeSpan.Zero, Interval);
+            return Task.CompletedTask;
+        }
+
+        public void Stop() => _timer?.Change(Timeout.Infinite, Timeout.Infinite);
+        public void Dispose() => _timer?.Dispose();
+    }
+}
---
+++ SET09102-2024-5/Services/SensorService.cs
@@ -0,0 +1,37 @@
+﻿// Services/SensorService.cs (or wherever you keep it)
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SET09102_2024_5.Data.Repositories;
+using SET09102_2024_5.Models;
+
+namespace SET09102_2024_5.Services
+{
+    public class SensorService
+    {
+        private readonly IRepository<Sensor> _sensorRepo;
+
+        public event Action<Sensor, DateTime?>? OnSensorUpdated;
+
+        public SensorService(IRepository<Sensor> sensorRepo)
+        {
+            _sensorRepo = sensorRepo;
+        }
+
+        public async Task<List<Sensor>> GetAllWithConfigurationAsync()
+        {
+            // assumes your repository eagerly loads Configuration via Include
+            return (await _sensorRepo.GetAllAsync()).ToList();
+        }
+
+        public async Task StartAsync(TimeSpan pollingInterval)
+        {
+            while (true)
+            {
+                var list = await GetAllWithConfigurationAsync();
+                foreach (var s in list)
+                    OnSensorUpdated?.Invoke(s, null);
+                await Task.Delay(pollingInterval);
+            }
+        }
+    }
+}
