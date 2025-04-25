d2d9888165e663279830db5193d3576588e33f6f
Illia <illiadovho@gmail.com>
2025-04-25 11:48:52 +0100

    Modified main page with button to redirect to map page

--- SET09102-2024-5/Views/MainPage.xaml.cs
+++ SET09102-2024-5/Views/MainPage.xaml.cs
@@ -1,12 +1,22 @@
+using Microsoft.Maui.Controls;
+using System;
+using SET09102_2024_5.Views;
 using SET09102_2024_5.ViewModels;
 
 namespace SET09102_2024_5.Views
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage()
+        private readonly MapPage _mapPage;
+
+        public MainPage(MainPageViewModel vm, MapPage mapPage)
         {
             InitializeComponent();
+            BindingContext = vm;
+            _mapPage = mapPage;
         }
+
+        private async void OnViewStatusMapClicked(object sender, EventArgs e)
+            => await Navigation.PushAsync(_mapPage);
     }
 }
