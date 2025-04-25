e60920057bc73192d855a6b1ec485d58211bb95b
Illia <illiadovho@gmail.com>
2025-04-25 11:45:16 +0100

    Added sensor repository

---
+++ SET09102-2024-5/Repositories/ISensorRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SET09102_2024_5.Models;
+
+namespace SET09102_2024_5.Data.Repositories
+{
+    public interface ISensorRepository : IRepository<Sensor>
+    {
+        Task<List<Sensor>> GetAllWithConfigurationAsync();
+    }
+}
---
+++ SET09102-2024-5/Repositories/SensorRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SET09102_2024_5.Models;
+
+namespace SET09102_2024_5.Data.Repositories
+{
+    public class SensorRepository : Repository<Sensor>, ISensorRepository
+    {
+        private readonly SensorMonitoringContext _ctx;
+        public SensorRepository(SensorMonitoringContext ctx) : base(ctx)
+            => _ctx = ctx;
+
+        public Task<List<Sensor>> GetAllWithConfigurationAsync() =>
+            _ctx.Sensors
+                .Include(s => s.Configuration)
+                .AsNoTracking()
+                .ToListAsync();
+    }
+}
