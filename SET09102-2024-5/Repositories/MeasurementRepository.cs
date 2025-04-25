f62a11fda38e599ee4cc3a33ae750ee011d318b3
Illia <illiadovho@gmail.com>
2025-04-25 11:45:04 +0100

    Added measurement repository

---
+++ SET09102-2024-5/Repositories/MeasurementRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SET09102_2024_5.Models;
+
+namespace SET09102_2024_5.Data.Repositories
+{
+    public class MeasurementRepository : Repository<Measurement>, IMeasurementRepository
+    {
+        private readonly SensorMonitoringContext _ctx;
+        public MeasurementRepository(SensorMonitoringContext ctx) : base(ctx)
+            => _ctx = ctx;
+
+        public Task<List<Measurement>> GetSinceAsync(DateTime since) =>
+            _ctx.Measurements
+                .Include(m => m.Measurand)
+                    .ThenInclude(q => q.Sensor)
+                .AsNoTracking()
+                .Where(m => m.Timestamp > since)
+                .ToListAsync();
+    }
+}
