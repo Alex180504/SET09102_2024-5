a968ca720f56f936fc16ca37fbe0211749dfe3cc
Illia <illiadovho@gmail.com>
2025-04-27 01:05:37 +0100

    Fixed bug of not seeing pins until first zoom

--- SET09102-2024-5/ViewModels/MapViewModel.cs
+++ SET09102-2024-5/ViewModels/MapViewModel.cs
@@ -132,6 +132,7 @@ namespace SET09102_2024_5.ViewModels
 
                 // Swap new features into the layer
                 _pinLayer.Features = features;
+                Map.Refresh();
             }
             catch (Exception ex)
             {
