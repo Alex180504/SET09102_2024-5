22dd9faaea74d5b8077ba200b0325d4f779944fb
Alex180504 <125076392+Alex180504@users.noreply.github.com>
2025-04-28 07:11:19 +0100

    Add converters for sensor locator

---
+++ SET09102-2024-5/Converters/IntToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace SET09102_2024_5.Converters
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int count && parameter is string minValue)
+            {
+                if (int.TryParse(minValue, out int min))
+                {
+                    return count >= min;
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
